bb427e6d973d4704e93d9915e8d9ff01243e869d
[dyad] <git@dyad.sh>
2025-07-23 20:09:43 -0300

    [dyad] Fixing build by removing mobile dependencies - wrote 3 file(s), deleted 3 file(s) + extra files edited outside of Dyad

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.lunarwalrusroam',
-  appName: 'Almoxarifado Yees!',
-  webDir: 'dist'
-};
-
-export default config;
