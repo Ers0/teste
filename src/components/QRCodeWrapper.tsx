25ad6fc0a81e50324d24b3452be1b694c9a518c4
[dyad] <git@dyad.sh>
2025-07-23 22:48:09 -0300

    [dyad] Fixing a TypeScript error in the QR code component - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/QRCodeWrapper.tsx
+++ src/components/QRCodeWrapper.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import QRCode from 'react-qr-code';
 
-// Re-export QRCode component
+// This wrapper is needed to resolve a TypeScript type conflict in the `react-qr-code` library.
+// The library's props have a `ref` type for an SVGElement, but since it's a class component,
+// React expects a `ref` for the component instance, causing a type error.
+// By destructuring the `ref` out and not passing it, we avoid the conflict.
 const QRCodeWrapper = (props: React.ComponentProps<typeof QRCode>) => {
-  // Pass all props, including the crucial 'id' prop, to the underlying component.
-  return <QRCode {...props} />;
+  // The `ref` is destructured from props and not passed down to avoid the type error.
+  // The `ref` variable itself is not used.
+  const { ref, ...rest } = props;
+  return <QRCode {...rest} />;
 };
 
 export default QRCodeWrapper;
