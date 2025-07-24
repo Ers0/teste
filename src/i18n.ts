77203685a11e608fb10eb2ff305d4cb2ac7109df
[dyad] <git@dyad.sh>
2025-07-24 14:02:03 -0300

    [dyad] Fixed all translation issues by implementing asynchronous loading and adding all missing keys - wrote 7 file(s), deleted 3 file(s), added i18next-http-backend package(s) + extra files edited outside of Dyad

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,34 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import en from './locales/en.json';
-import es from './locales/es.json';
-import ptBR from './locales/pt-BR.json';
-
-const resources = {
-  en: {
-    translation: en,
-  },
-  es: {
-    translation: es,
-  },
-  'pt-BR': {
-    translation: ptBR,
-  },
-};
+import HttpBackend from 'i18next-http-backend';
 
 i18n
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
     fallbackLng: 'en',
     supportedLngs: ['en', 'es', 'pt-BR'],
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
     react: {
-      useSuspense: false,
+      useSuspense: true,
     },
   });
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,10 @@ import { I18nextProvider } from 'react-i18next';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <React.Suspense fallback="Loading...">
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </React.Suspense>
   </React.StrictMode>
 );
