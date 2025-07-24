1fb80dc6dfe77bce4927e6c77b19ededee18994a
[dyad] <git@dyad.sh>
2025-07-23 21:32:25 -0300

    [dyad] Fixing missing translations by using React.Suspense - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
@@ -6,7 +7,9 @@ import { I18nextProvider } from 'react-i18next'; // Import I18nextProvider
 import i18n from './i18n'; // Import the i18n instance
 
 createRoot(document.getElementById("root")!).render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>
+  <React.Suspense fallback="Loading...">
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.Suspense>
 );
