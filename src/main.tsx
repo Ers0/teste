875e2fd2dd539b6c576505737ceb9cef8b22622b
[dyad] <git@dyad.sh>
2025-07-24 13:53:17 -0300

    [dyad] Fixed all translation issues - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import './i18n'; // This import runs the init code
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
